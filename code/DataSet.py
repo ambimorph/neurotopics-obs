7a7b42495fda7e7fabbf982e1194ea2fff6fdf15
Amber on abhyasa <amber@abhyasa>
2013-05-26 08:52:27 -0600

    Make more explicit variable name.

--- code/DataSet.py
+++ code/DataSet.py
@@ -11,11 +11,11 @@ class DataSet:
     the img.
     """
 
-    def __init__(self, neurosynth_dataset, img, word_counts_file):
+    def __init__(self, neurosynth_dataset, image_file_name, word_counts_file):
 
         self.word_subset = neurosynth_dataset.get_feature_names()
         self.dois = list(neurosynth_dataset.feature_table.ids)
-        self.average_activation = nsar.average_within_regions(neurosynth_dataset, img)
+        self.average_activation = nsar.average_within_regions(neurosynth_dataset, image_file_name)
         self.get_word_subset_counts(self.word_subset, word_counts_file)
 
     def get_word_subset_counts(self, word_subset, word_counts_file):
