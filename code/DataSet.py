8613219315588f1aff988bff10a67039cd4b2326
Amber on abhyasa <amber@abhyasa>
2013-05-26 08:56:33 -0600

    Added commandline interface to class.

--- code/DataSet.py
+++ code/DataSet.py
@@ -21,7 +21,12 @@ class DataSet:
     def get_word_subset_counts(self, word_subset, word_counts_file):
         pass
 
-    
 
+if __name__ == '__main__':
+    import sys, pickle
+    pickle_file_name = sys.argv[1]
+    image_file_name = sys.argv[2]
+    word_counts_file = sys.argv[3]
+    neurosynth_dataset = pickle.load(open(pickle_file_name, 'rb'))
 
-        
+    dataset = Dataset(neurosynth_dataset, image_file_name, word_counts_file)
